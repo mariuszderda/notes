284dd1d50fc953ca08164e15ee728f549b1e8ae7
Mariusz Derda <mariusz.derda@gmail.com>
2021-07-08 01:31:52 +0200

    First commit - configure eslint prettier husky lint-staged

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,15 +8,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code> src/App.js </code>
+          and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ea earum eum eveniet ex exercitationem
+          facere ipsa nemo nihil pariatur quibusdam quisquam reprehenderit rerum totam ut, vitae voluptatum! A dolore
+          est eveniet facilis, magnam odio quos rem suscipit tempora totam. Alias aspernatur corporis ea nam placeat
+          provident quos tenetur vero! Cumque, facere facilis id itaque laudantium officia ratione rem repellendus!
+          Delectus mollitia natus placeat, quae quam quisquam! Aliquam aliquid, amet assumenda commodi consectetur,
+          consequuntur delectus doloremque ducimus eaque eligendi enim error esse ex exercitationem incidunt iure
+          laudantium minus nulla officia optio, porro possimus quae quibusdam sit velit veniam voluptatibus. Accusamus,
+          asperiores blanditiis, delectus deserunt distinctio eligendi eum exercitationem in minus nostrum numquam odit
+          quaerat quam reprehenderit sed? Asperiores, modi, sit.
         </a>
       </header>
     </div>
